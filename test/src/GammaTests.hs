27a0d23de33b5b957ab80624d65fc8971b954bc3
James Cook <mokus@deepbondi.net>
2010-07-08 00:47:49 +0000

    more tests

--- test/src/GammaTests.hs
+++ test/src/GammaTests.hs
@@ -24,6 +24,18 @@ tests =
         , testGroup "Complex Float"  (complexGammaTests (16636 * eps :: Float))
         , testGroup "Complex Double" (complexGammaTests (16636 * eps :: Double))
         ]
+    , testGroup "lnGamma"
+        [ testGroup "Float"          (realLogGammaTests    (1024  * eps :: Float))
+        , testGroup "Double"         (realLogGammaTests    (512   * eps :: Double))
+        , testGroup "Complex Float"  (complexLogGammaTests (16636 * eps :: Float))
+        , testGroup "Complex Double" (complexLogGammaTests (16636 * eps :: Double))
+        ]
+    , testGroup "lnFactorial"
+        [ testGroup "Float"          (logFactorialTests abs       (eps :: Float))
+        , testGroup "Double"         (logFactorialTests abs       (eps :: Double))
+        , testGroup "Complex Float"  (logFactorialTests magnitude (eps :: Float))
+        , testGroup "Complex Double" (logFactorialTests magnitude (eps :: Double))
+        ]
     ]
 
 realGammaTests eps = gammaTests abs id (const 0) eps ++
@@ -45,7 +57,10 @@ complexGammaTests eps = gammaTests magnitude realPart imagPart eps ++
     ]
     where
         infix 4 ~=
-        x ~= y = (errBy magnitude x y <= eps)
+        x ~= y 
+            =  absErr <= eps
+            || absErr <= eps * min (magnitude x) (magnitude y)
+            where absErr = magnitude (x-y)
 
 gammaTests abs real imag eps =
     [ testProperty "increment arg" $ \x ->
@@ -71,10 +86,62 @@ gammaTests abs real imag eps =
             || absErr <= eps * min (abs x) (abs y)
             where absErr = abs (x-y)
 
-err a b = errBy abs a b
-errBy abs a b 
-    | isNaN relErr  = absErr
-    | otherwise     = min absErr relErr
+logGammaTests abs real imag eps =
+    [ testProperty "increment arg" $ \x ->
+        let gam = lnGamma (x+1)
+         in real x > 0 && isSane (abs gam)
+            ==> gam - log x ~= lnGamma x
+             || gam ~= log x + lnGamma x
+    , testProperty "reflect" $ \x ->
+        abs x > 0 ==>
+        let a = lnGamma x
+            b = lnGamma (1 - x)
+            c = log (pi / sin (pi * x))
+         in all (isSane.abs) [a,b,c] 
+            ==> a + b ~= c
+             || a ~= c - b
+             || b ~= c - a
+             || a - b - log (sin(pi*x)) ~= pi
+    ]
     where
-        absErr = abs (a-b)
-        relErr = absErr / max (abs a) (abs b)
+        infix 4 ~=
+        x ~= y 
+            =  absErr <= eps
+            || absErr <= eps * min (abs x) (abs y)
+            where absErr = abs (x-y)
+
+
+realLogGammaTests eps = logGammaTests abs id (const 0) eps ++
+    [ testProperty "between factorials" $ \(Positive x) -> 
+        let gam x = sum $ map (log.fromInteger) [1..x-1]
+            gamma_x = lnGamma x `asTypeOf` eps
+         in x > 2 && isSane gamma_x
+            ==> gam (floor x) <= gamma_x && gamma_x <= gam (ceiling x)
+    ]
+
+complexLogGammaTests eps = logGammaTests magnitude realPart imagPart eps ++
+    [ testProperty "real argument" $ \(Positive x) ->
+        let z = x :+ 0
+            gam = lnGamma x
+         in isSane gam ==> (gam :+ 0) ~= lnGamma z
+    ]
+    where
+        infix 4 ~=
+        x ~= y 
+            =  absErr <= eps
+            || absErr <= eps * min (magnitude x) (magnitude y)
+            where absErr = magnitude (x-y)
+
+
+logFactorialTests abs eps =
+    [ testProperty "agrees with lnGamma" $ \x ->
+        let gam = lnGamma (fromInteger x + 1)
+         in isSane (abs gam)
+            ==> gam ~= lnFactorial x
+    ]
+    where
+        infix 4 ~=
+        x ~= y 
+            =  absErr <= eps
+            || absErr <= eps * min (abs x) (abs y)
+            where absErr = abs (x-y)
--- test/src/IncGammaTests.hs
+++ test/src/IncGammaTests.hs
@@ -1,5 +1,40 @@
 module IncGammaTests where
 
+import GammaTests (eps, isSane)
+
+import Data.Complex
+import Math.Gamma
+import Test.Framework (testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck
+
 tests = 
-    [
+    [ testGroup "incomplete gamma"
+        [ testGroup "Float"  (incompleteGammaTests (128 * eps :: Float))
+        , testGroup "Double" (incompleteGammaTests (512 * eps :: Double))
+        ]
     ]
+
+incompleteGammaTests eps =
+    [ testProperty "lowerGamma + upperGamma" $ \s x ->
+        let a = lowerGamma s x
+            b = upperGamma s x
+            c = gamma s
+         in all isSane [a,b,c] 
+            ==> a+b ~= c
+             || a ~= c-b
+             || b ~= c-a
+    , testProperty "p + q" $ \s x ->
+        let a = p s x
+            b = q s x
+         in all isSane [a,b] 
+            ==> a+b ~= 1
+             || a   ~= 1-b
+             || b   ~= 1-a
+    ]
+    where
+        infix 4 ~=
+        x ~= y 
+            =  absErr <= eps
+            || absErr <= eps * min (abs x) (abs y)
+            where absErr = abs (x-y)
